acd6e0d9303b40a056b7a57601ae3ace94237cad
Erik Westlund <edbwestlund@gmail.com>
2026-02-03 13:04:48 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        cors: true,
+        host: 'localhost',
+        port: 5178,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
